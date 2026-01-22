da28a84667eda6a56fdac32ba316c92054292edc
sahebk <159716068+sahebk@users.noreply.github.com>
2026-01-22 13:54:40 +0530

    Implement array reversal in ReverseArray class

---
+++ ReverseArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void reverse(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        reverse(arr);
+
+        System.out.println("Reversed array:");
+        for (int x : arr) System.out.print(x + " ");
+
+        sc.close();
+    }
+}
